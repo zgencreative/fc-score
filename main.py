0c83a065b369f026ae4d3e8bfbbcfd2261669076
dev <37302546+kurokocreative@users.noreply.github.com>
2025-02-06 14:08:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,8 @@ import requests
 import datetime
 from flask_cors import CORS
 
-
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
-
-# app.config["PREFERRED_URL_SCHEME"] = "http"
-# app.config["SERVER_NAME"] = "score.mansionsportsfc.com"
 
 # BACKEND
 
